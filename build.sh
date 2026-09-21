1371e855ed422438e333386d3cd32590910d7d55
mrkmntal <marky611@gmail.com>
2026-09-21 11:29:19 -0400

    Rename package from hurd-socket-stat to hss

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 rm -rf dist && mkdir dist
 dpkg-buildpackage -us -uc
-mv ../hurd-socket-stat_* dist/ 2>/dev/null || true
+mv ../hss_* dist/ 2>/dev/null || true
 echo "Build artifacts in dist/"
